cc301dff9a25fcf6f46ec7b12304e27ab9e7fcec
Harumb1 <miroslav.mihaylov.pcj@gmail.com>
2022-01-21 19:56:33 +0200

    Commit Message

--- src/main/java/beecraft/beecraft/commands/BeeCommand.java
+++ src/main/java/beecraft/beecraft/commands/BeeCommand.java
@@ -4,13 +4,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 
 public class BeeCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
-        sender.sendMessage("Bees");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This is a player only command!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        player.getWorld().spawnEntity(player.getLocation(), EntityType.BEE);
+
+
+        //spawnEntity(location, entity)
+
 
         return false;
     }
